da6267aa83dc2ac92424c35d427efff4ffe13971
perryhow13 <youngho.kang@kaist.ac.kr>
2017-08-22 20:25:06 +0900

    Submitting third problem python script file but empty

---
+++ ex3/largest_prime_factor.py
@@ -0,0 +1,17 @@
+
+
+
+"""
+<Definition of prime number>:
+
+A prime number can be divided evenly only by 1, or itself. Also it must be a
+whole number greater than 1.
+
+
+<Definition of prime factor>:
+
+A factor that is a prime number. For example, 3 x 2 = 6. 3 and 2 are factors
+
+"""
+
+
